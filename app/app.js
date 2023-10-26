5e1bacc4ee27ef8b52619dab761fc8c2ac1ec1e0
AnBoran <boransama@gmail.com>
2023-10-26 18:58:05 +0900

    프론트단 JavaScript파일 생성

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); // ejs는 많이 사용하는 view엔진 중 하나로 HTML과 비슷
+app.use(express.static(`${__dirname}/src/public`)); // 정적 경로 추가를 위한 미들웨어 추가. app.js의 현재위치/src/public를 정적 경로로 추가하겠다는 뜻
 
 app.use("/", home); // use는 미들웨어를 등록하는 메서드
 
