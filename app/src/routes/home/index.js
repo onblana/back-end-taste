c1ba8580b522418168d404858a94827e803ee9ce
AnBoran <boransama@gmail.com>
2023-11-02 18:48:43 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 // API?
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router; // 외부에서 사용 가능하도록 내보내기 함
